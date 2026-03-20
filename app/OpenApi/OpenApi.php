c75a9a71d7361055b414aebefd066a7595db6ad3
root <root@srv1484277.hstgr.cloud>
2026-03-20 07:50:23 +0000

    feat: add l5-swagger and health endpoint; update routes and lockfile

---
+++ app/OpenApi/OpenApi.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @OA\Info(
+ *   title="Queue System API",
+ *   version="1.0.0"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *   securityScheme="bearerAuth",
+ *   type="http",
+ *   scheme="bearer",
+ *   bearerFormat="JWT"
+ * )
+ */
+``
